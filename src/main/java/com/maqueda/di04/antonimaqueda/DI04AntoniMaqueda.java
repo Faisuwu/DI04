719e5090fef6816034b165fe584628f5bfba0931
Faisuwu <105352928+Faisuwu@users.noreply.github.com>
2025-01-25 17:39:00 +0100

    primer commit

---
+++ src/main/java/com/maqueda/di04/antonimaqueda/DI04AntoniMaqueda.java
@@ -0,0 +1,10 @@
+//DI04-AntoniMaqueda.java
+
+package com.maqueda.di04.antonimaqueda;
+
+public class DI04AntoniMaqueda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
